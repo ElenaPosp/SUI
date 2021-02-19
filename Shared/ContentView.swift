65f3ecf50db08d4c05ecfc888d5116d5ee55ff9e
e.pospelova <e.pospelova@nb0453.tcsbank.ru>
2021-02-19 15:12:13 +0300

    Landmark

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,23 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Hello, Elena and swiftUI!!!")
-                .font(.title)
-                .fontWeight(.light)
-                .foregroundColor(.orange)
-                .lineLimit(0)
+        VStack {
+            MapView()
+                .frame(height: 300)
+                .ignoresSafeArea(edges: .top)
 
-            HStack {
-                Text("Офигенно крутая весч")
-                    .font(.subheadline)
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
                     .fontWeight(.light)
-                Spacer()
-                Text("Рили")
-            }
-        }
-        .padding()
+                    .lineLimit(0)
 
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+
+                Divider()
+
+                Text("About Turtle Rock")
+                    .font(.title2)
+                    .fontWeight(.light)
+                Text("Descriptive text goes here.")
+            }
+            .padding()
+
+            Spacer()
+        }
     }
 }
 
---
+++ Shared/Landmark.swift
@@ -0,0 +1,8 @@
+//
+//  Landmark.swift
+//  SwiftUIExample
+//
+//  Created by e.pospelova on 13.02.2021.
+//
+
+import Foundation

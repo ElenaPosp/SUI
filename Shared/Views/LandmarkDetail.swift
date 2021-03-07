f315bef22335559ba3a2bbb7852331894a9cb73c
e.pospelova <pospelova_elena_al@mail.ru>
2021-03-07 13:16:20 +0300

    Set data from json

--- Shared/Views/LandmarkDetail.swift
+++ Shared/Views/LandmarkDetail.swift
@@ -8,45 +8,51 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+
+    let landmark: Landmark
+
     var body: some View {
-        VStack {
-            MapView()
+        ScrollView {
+            MapView(coordinate: landmark.locationCoordinate)
                 .frame(height: 300)
 
-            CircleImage()
+            CircleImage(image: landmark.image)
                 .offset(y: -130)
                 .padding(.bottom, -130)
 
             VStack(alignment: .leading) {
-                Text("Turtle Rock")
+                Text(landmark.name)
                     .font(.title)
                     .fontWeight(.light)
                     .lineLimit(0)
 
                 HStack {
-                    Text("Joshua Tree National Park")
+                    Text(landmark.park)
                     Spacer()
-                    Text("California")
+                    Text(landmark.state)
                 }
                 .font(.subheadline)
                 .foregroundColor(.secondary)
 
                 Divider()
 
-                Text("About Turtle Rock")
+                Text("About \(landmark.name)")
                     .font(.title2)
                     .fontWeight(.light)
-                Text("Descriptive text goes here.")
+                Text(landmark.description)
             }
             .padding()
 
-            Spacer()
+//            Spacer()
         }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkDetail()
+        var dec = Decoder()
+        LandmarkDetail(landmark: dec.landmarks[3])
     }
 }
